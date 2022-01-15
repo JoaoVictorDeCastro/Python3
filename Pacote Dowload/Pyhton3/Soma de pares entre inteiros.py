8a0c0e517aa73b3c5da9e8d43f119c810dae75cb
João Victor De Castro <joaovictordecastrofaria@gmail.com>
2022-01-14 21:36:50 -0300

    .

--- Pacote Dowload/Pyhton3/Soma de pares entre inteiros.py
+++ Pacote Dowload/Pyhton3/Soma de pares entre inteiros.py
@@ -3,4 +3,4 @@ for num in range (1,7):
     c = int(input('Digite um número inteiro:'))
     if c%2 == 0:
         cont = cont+c
-print(cont)
+print('A soma dos valores pares dos números digitados é {}' .format(cont))
