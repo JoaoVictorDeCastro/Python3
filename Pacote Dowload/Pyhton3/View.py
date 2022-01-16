2e9592b9d4930fa1f3128fd1cbfa47a234dc8b94
João Victor De Castro <joaovictordecastrofaria@gmail.com>
2022-01-15 23:52:27 -0300

    View - Final version

--- Pacote Dowload/Pyhton3/View.py
+++ Pacote Dowload/Pyhton3/View.py
@@ -1,7 +1,13 @@
-for c
-    nome = input('Qual o seu nome?')
-    idade = int(input('Qual a sua idade?'))
-    sexo = input('Qual o seu gênero? [M,F]')
+for c in range(0, 100000):
+    nome = input('Qual o seu nome? ')
+    idade = int(input('Qual a sua idade? '))
+    sexo = input('Qual o seu gênero? [M,F] ').strip().upper()
     print('----- Informações -----')
-    print('Nome: {}' .format(nome))
-    print('Idade: {}' .format(idade))
+    print('Nome: {}.' .format(nome))
+    print('Idade: {}.' .format(idade))
+    if sexo == 'M':
+        print('Gênero: Masculino.')
+    elif sexo == 'F':
+        print('Gênero: Feminino.')
+    else:
+        print('Gênero: Prefere não informar.')
