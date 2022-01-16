137dca2b3812d9a2a630af3bb7cadba3370d632f
João Victor De Castro <joaovictordecastrofaria@gmail.com>
2022-01-15 23:48:18 -0300

    .

--- Pacote Dowload/Pyhton3/View.py
+++ Pacote Dowload/Pyhton3/View.py
@@ -1,3 +1,7 @@
-nome = input('Qual o seu nome?')
-idade = int(input('Qual a sua idade?'))
-sexo = input('Qual o seu gênero? [M,F]')
+for c
+    nome = input('Qual o seu nome?')
+    idade = int(input('Qual a sua idade?'))
+    sexo = input('Qual o seu gênero? [M,F]')
+    print('----- Informações -----')
+    print('Nome: {}' .format(nome))
+    print('Idade: {}' .format(idade))
