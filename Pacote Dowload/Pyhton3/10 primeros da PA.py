025444546559b10530740a137fc985061f7205b2
João Victor De Castro <joaovictordecastrofaria@gmail.com>
2022-01-21 22:09:08 -0300

    tests

--- Pacote Dowload/Pyhton3/10 primeros da PA.py
+++ Pacote Dowload/Pyhton3/10 primeros da PA.py
@@ -1,6 +1,6 @@
 primeiro = int(input('Primeiro termo: '))
-razao = int(input('Razão: '))
-decimo = primeiro + (10-1)*razao
-for c in range (primeiro, decimo+razao, razao):
+razão = int(input('Razão: '))
+decimo = primeiro + (10-1)*razão
+for c in range (primeiro, decimo+razão, razão):
     print('{}' .format(c), end=' -> ')
 print('ACABOU')
