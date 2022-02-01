f235bf29a959f001a5fb7b22718d5e53c83d140d
João Victor De Castro <joaovictordecastrofaria@gmail.com>
2022-01-31 21:09:24 -0300

    Finished GUI

--- Pacote Dowload/Pyhton3/Cadastro GUI.py
+++ Pacote Dowload/Pyhton3/Cadastro GUI.py
@@ -1,1 +1,17 @@
 from PySimpleGUI import PySimpleGUI as sg
+
+sg,theme('Reddit')
+layout - [
+    [sg.text('Usuário'),sg.Input(key='usuario', size=(20,1))],
+    [sg.text('Senha'),sg.Input(key='senha',password_char='*', size=(20,1))],
+    [sg.Checkbox('Salvar o login?')],
+    [sg.Button('Entrar')]
+]
+janela = sg.Window('Tela de Login', layout)
+while True:
+    eventos, valores = janela.read()
+    if eventos == sg.WINDOW_CLOSED:
+        break
+    if eventos == 'Entrar':
+        if valores['usuario'] == 'João' and valores['senha'] == '12345':
+            print('Bem vindo ao mundo python!')
