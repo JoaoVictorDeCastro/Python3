f1ce0a50204b6d16a494f4b041ea3252d9052231
João Victor De Castro <joaovictordecastrofaria@gmail.com>
2022-02-08 22:42:07 -0300

    For 1

---
+++ Pacote Dowload/Pyhton3/Tuplas.py
@@ -0,0 +1,6 @@
+# Tuplas são IMUTÁVEIS
+lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
+print(lanche[:2])
+for comida in lanche:
+    print(f'Eu vou comer {comida}.')
+print('Comi para caramba!')
