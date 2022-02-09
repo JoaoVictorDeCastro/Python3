70ff798a88d67f4091fb5e7d31a14e3843fd46a7
João Victor De Castro <joaovictordecastrofaria@gmail.com>
2022-02-08 22:42:21 -0300

    for 2

--- Pacote Dowload/Pyhton3/Tuplas.py
+++ Pacote Dowload/Pyhton3/Tuplas.py
@@ -4,3 +4,6 @@ print(lanche[:2])
 for comida in lanche:
     print(f'Eu vou comer {comida}.')
 print('Comi para caramba!')
+for cont in range(0, len(lanche)):
+    print(f'Eu vou comer {lanche[cont]} na posição {cont}')
+print('Comi para caramba!')
