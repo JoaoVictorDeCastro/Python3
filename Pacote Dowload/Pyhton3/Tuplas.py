2fdf8fa6027b97b74941810a7c307f5cf003997e
João Victor De Castro <joaovictordecastrofaria@gmail.com>
2022-02-08 22:59:20 -0300

    Fim da aula de Tuplas

--- Pacote Dowload/Pyhton3/Tuplas.py
+++ Pacote Dowload/Pyhton3/Tuplas.py
@@ -10,3 +10,15 @@ print('Comi para caramba!')
 for pos, comida in enumerate(lanche):
     print(f'Eu comer {comida} na posição {pos}')
 print('Comi para caramba!')
+print(sorted(lanche))
+a = (2, 5, 4)
+b = (5, 8, 1, 2)
+c = b + a
+print(c)
+print(len(c))
+print(c.count(5))
+print(c.index(8))
+print(c.index(5, 1))
+pessoa = ('Gustavo', 39, 'M', 99.88)
+del pessoa
+
